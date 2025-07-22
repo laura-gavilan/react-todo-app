94442d8f7a54623e532d5845fe979eb0918bb045
Laura Gavilan <lauragavilansabiote@hotmail.com>
2025-07-22 19:54:18 +0200

    feat(todos): add delete and favorite actions for todos

--- src/App.jsx
+++ src/App.jsx
@@ -45,12 +45,25 @@ export const App = () => {
         setTodos((prev) => [...prev, newTodo]);
     };
 
-    const toggleTodo = (id) => {
+    const onToggleTodo = (id) => {
         const updatedTodos = todos.map((todo) => {
-            return todo.id === id ? {...todo, done: !todo.done} : todo
+            return todo.id === id ? { ...todo, done: !todo.done } : todo
         });
 
         setTodos(updatedTodos);
+    };
+
+    const onToggleFavorite = (id) => {
+        const updatedTodos = todos.map((todo) => {
+            return todo.id === id ? { ...todo, favorite: !todo.favorite } : todo;
+        });
+
+        setTodos(updatedTodos);
+    };
+
+    const onDeleteTodo = (id) => {
+        const filtered = todos.filter(todo => todo.id !== id);
+        setTodos(filtered);
     };
 
     return (
@@ -58,7 +71,12 @@ export const App = () => {
             <Navigation activeTab={activeTab} setActiveTab={setActiveTab} />
             <h1 className="app-title">Mi lista de tareas</h1>
 
-            {activeTab === Tabs.TODOS && <TodoList toggleTodo={toggleTodo} todos={todos} />}
+            {activeTab === Tabs.TODOS && <TodoList
+                todos={todos}
+                onToggleTodo={onToggleTodo}
+                onDeleteTodo={onDeleteTodo}
+                onToggleFavorite={onToggleFavorite}
+            />}
 
             {activeTab === Tabs.FAVORITES && <h2>Pestaña: Favoritos</h2>}
 
--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -5,13 +5,34 @@ import "./TodoItem.css"
 //Favorito
 
 
-export const TodoItem = ({todo, toggleTodo})=> {
+export const TodoItem = ({ todo, onToggleTodo, onDeleteTodo, onToggleFavorite }) => {
     return (
-        <div className={`todo-item ${todo.done ? "done" : ""}`} onClick={() => toggleTodo(todo.id)}>
+        <div
+            className={`todo-item ${todo.done ? "done" : ""}`}
+            onClick={() => onToggleTodo(todo.id)}>
+
             <span className="todo-text">{todo.text}</span>
 
-            <div className="todo-actions"></div>
-        </div>
+            <div className="todo-actions">
+                <button
+                    className={`favorite-btn ${todo.favorite ? "active" : ""}`}
+                    onClick={(event) => {
+                        event.stopPropagation();
+                        onToggleFavorite(todo.id)
+                    }} >
 
-    )
-}
+                    {todo.favorite ? "⭐" : "⭐"}
+                </button>
+
+                <button
+                    className="delete-btn"
+                    title="Eliminar"
+                    onClick={(event) => {
+                        event.stopPropagation();
+                        onDeleteTodo(todo.id)
+                    }}>
+                </button>
+            </div>
+        </div>
+    );
+};
